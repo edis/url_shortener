8232371afd15e614c043380d55973573f2dd8d74
edis <edis@gonuler.com>
2020-06-22 16:07:25 -0400

    setup connect with mongo db

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import ssl
 from flask import Flask, render_template, request, redirect
+from flask_pymongo import pymongo
 
 app = Flask(__name__, template_folder='templates')
+
+client = pymongo.MongoClient("mongodb+srv://dbAdmin:ZUYONlfCtQC5WiBu@cluster0-d16zl.mongodb.net/url-shortener?retryWrites=true&w=majority", ssl_cert_reqs=ssl.CERT_NONE)
+# db = client["urls-shortener"]
+# col = db["example"]
+# mydict = {"name": "John", "address": "Highway 37" }
+#
+# x = col.insert_one(mydict)
 
 
 @app.route('/', methods=['GET', 'POST'])

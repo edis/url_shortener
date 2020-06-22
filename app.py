8897fdb6b123a42bc6f6955a4617974a05221cb9
edis <edis@gonuler.com>
2020-06-22 17:48:52 -0400

    update gitignore and store info in .env

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+import os
 import ssl
 from flask import Flask, render_template, request, redirect
 from flask_pymongo import pymongo
 from gen_slug import base62_slug
-from secrets import MONGO_DB_PASSWORD
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__, template_folder='templates')
 
-client = pymongo.MongoClient(f"mongodb+srv://dbAdmin:{MONGO_DB_PASSWORD}@cluster0-d16zl.mongodb.net/urls-redirects"
-                             f"?retryWrites=true&w=majority", ssl_cert_reqs=ssl.CERT_NONE)
+client = pymongo.MongoClient(f"mongodb+srv://{os.getenv('MONGO_DB_USER')}:{os.getenv('MONGO_DB_PASSWORD')}@{os.getenv('CLUSTER_NAME')}.mongodb.net/urls-redirects?retryWrites=true&w=majority", ssl_cert_reqs=ssl.CERT_NONE)
 
 db = client["urls-redirects"]
 

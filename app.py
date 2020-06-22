a3403df44d80ce5b7501283c72ccf31640ed342d
edis <edis@gonuler.com>
2020-06-22 16:34:21 -0400

    implement handling redirects

--- app.py
+++ app.py
@@ -5,27 +5,29 @@ from flask_pymongo import pymongo
 app = Flask(__name__, template_folder='templates')
 
 client = pymongo.MongoClient("mongodb+srv://dbAdmin:ZUYONlfCtQC5WiBu@cluster0-d16zl.mongodb.net/url-shortener?retryWrites=true&w=majority", ssl_cert_reqs=ssl.CERT_NONE)
-# db = client["urls-shortener"]
-# col = db["example"]
-# mydict = {"name": "John", "address": "Highway 37" }
-#
-# x = col.insert_one(mydict)
+db = client["urls-shortener"]
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # TODO: add url to database and return shortened url (shortening logic)
-        return
-    else:
-        return render_template('index.html')
+        url = request.form['init-url']
+        col = db["abc"]
+        url_dict = {"url": url}
+        col.insert(url_dict)
+    return render_template('index.html')
 
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def route(path):
-    # TODO: go to database and find correct url to redirect to
-    return
+    try:
+        redirect_url = db[path].find_one()["url"]
+        return redirect(redirect_url)
+    except TypeError:
+        # TODO: render 404 page
+        return render_template('index.html')
 
 
 if __name__ == "__main__":

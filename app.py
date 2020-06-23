a5aa96336928ccaaa3d45ec6513c3883c257a877
edis <edis@gonuler.com>
2020-06-23 14:58:14 -0400

    fix url storage in db and add styling

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from gen_slug import base62_slug
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 client = pymongo.MongoClient(f"mongodb+srv://{os.getenv('MONGO_DB_USER')}:{os.getenv('MONGO_DB_PASSWORD')}@"
                              f"{os.getenv('CLUSTER_NAME')}.mongodb.net/urls-redirects?retryWrites=true&w=majority",
@@ -20,21 +20,21 @@ def index():
     if request.method == 'POST':
         url = request.form['init-url']
 
-        # TODO: factor counter and document logic
-
         counter_db = client["counter"]["counter"]
         counter_num = counter_db.find_one()["counter"]
 
         slug = base62_slug(counter_num)
+
         counter_db.update_one({"counter": counter_num}, {"$set": {"counter":  counter_num + 1}})
 
         col = db[slug]
         url_dict = {"url": url}
         col.insert(url_dict)
 
-        # TODO: render page with url
+        return render_template('url.html', slug=slug)
 
-    return render_template('index.html')
+    elif request.method == 'GET':
+        return render_template('index.html')
 
 
 @app.route("/", defaults={"path": ""})
@@ -44,8 +44,7 @@ def route(path):
         redirect_url = db[path].find_one()["url"]
         return redirect(redirect_url)
     except TypeError:
-        # TODO: render 404 page
-        return render_template('index.html')
+        return render_template('404.html')
 
 
 if __name__ == "__main__":
